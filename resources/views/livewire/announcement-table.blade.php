2723af9b3da2cd1ca60a853ae7df60050f85586d
Raizo-03 <123054277+Raizo-03@users.noreply.github.com>
2025-03-26 12:49:02 +0800

    fix bug announcement

--- resources/views/livewire/announcement-table.blade.php
+++ resources/views/livewire/announcement-table.blade.php
@@ -20,7 +20,7 @@
 
         <h2 class="text-2xl font-bold mb-4">Add Announcement</h2>
 
-        <form wire:submit.prevent="saveAnnouncement" enctype="multipart/form-data">
+        <form wire:submit.prevent="saveAnnouncement" method="POST" enctype="multipart/form-data">
             <!-- Image Upload Preview -->
             <div class="mb-4">
                 @if ($image)
